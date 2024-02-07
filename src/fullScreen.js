e541bbc31d047da2fd42cc6c7668ada145e2ea1b
NovakRoman <roman.ss.novak@gmail.com>
2024-02-07 18:35:12 +0200

    added console: error()

--- src/fullScreen.js
+++ src/fullScreen.js
@@ -6,7 +6,9 @@ function toggleFullscreen(){
         container.style.background = 'white';
         container.style.color = 'black';
     } else {
-        container.requestFullscreen();
+        container.requestFullscreen().catch((err) => {
+            console.error('Error atempting to enable fullscreen:', err);
+        });
         container.style.background = 'black';
         container.style.color = 'white';
     }
@@ -16,3 +18,6 @@ const button = document.querySelector('#toggleFullScreen');
 button.addEventListener('click', toggleFullscreen);
 
 
+//  * Якщо ні, викличте element.requestFullscreen(), щоб ввести контейнер в повноекранний режим.
+//  * Обробіть помилки, якщо є такі.
+//  * Змініть стилі контейнера при вході та виході з повноекранного режиму (змініть фон та кольори тексту - element.style.background = 'black'; element.style.color = 'white';)
